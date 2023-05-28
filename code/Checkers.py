eae4f32d7bd281aef3e1ff29eec20d97739f9c66
ettorw <vero.veronese.0@outlook.com>
2023-05-28 21:09:28 +0200

    commit

---
+++ code/Checkers.py
@@ -0,0 +1,11 @@
+from board import Board
+
+run = True
+board = Board()
+move = [[None, None], [None, None]]
+print("==========| CheckerPY |==========")
+
+while run:
+    print(board.print_board())
+    print(board.piece_moves(2, 1))
+    input()
