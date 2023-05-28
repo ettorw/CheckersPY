107185216d5a4f6a6b887848f89e4d11ccf341ea
ettorw <vero.veronese.0@outlook.com>
2023-05-28 23:01:08 +0200

    interface and other stuff

--- code/Checkers.py
+++ code/Checkers.py
@@ -1,5 +1,105 @@
+import sys
+from PyQt5.QtGui import QColor, QBrush, QPen, QPainter
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt, QPoint
+
 from Board import Board
 
+board = Board()
+
+class CheckersBoard(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.piece_size = 120
+        self.setGeometry(500, 200, 8 * self.piece_size, 8 * self.piece_size)
+        self.setWindowTitle("Checkers Board")
+        
+        self.selected_piece = None
+        self.selected_piece_pos = None
+        
+        self.show()
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+
+        for row in range(8):
+            for col in range(8):
+                x = col * self.piece_size
+                y = row * self.piece_size
+
+                if (row + col) % 2 == 0:
+                    painter.setBrush(QColor("#999999"))
+                else:
+                    painter.setBrush(QColor("#111111"))
+
+                painter.drawRect(x, y, self.piece_size, self.piece_size)
+
+                piece = board.board[row][col]
+                if piece != 0 and piece != -1:
+                    if piece == 1:
+                        color = QColor("#63E7FE")
+                    elif piece == 2:
+                        color = QColor("#d90511")
+                    elif piece == 3:
+                        color = QColor("#63E7FE")
+                    elif piece == 4:
+                        color = QColor("#d90511")
+
+                    painter.setBrush(QBrush(color))
+                    painter.setPen(QPen(Qt.black, 2))
+                    painter.drawEllipse(x + 5, y + 5, self.piece_size - 10, self.piece_size - 10)
+
+        painter.end()
+
+    def mousePressEvent(self, event):
+        x = event.x()
+        y = event.y()
+
+        row = y // self.piece_size
+        col = x // self.piece_size
+
+        piece = board.board[row][col]
+        if piece != 0 and piece != -1:
+            self.selected_piece = piece
+            self.selected_piece_pos = (row, col)
+
+    def mouseMoveEvent(self, event):
+        if self.selected_piece is not None:
+            x = event.x()
+            y = event.y()
+
+            row = y // self.piece_size
+            col = x // self.piece_size
+
+            if board.is_valid_move(self.selected_piece_pos, [row, col]):
+                self.selected_piece_pos = (row, col)
+                self.update()
+
+    def mouseReleaseEvent(self, event):
+        if self.selected_piece is not None:
+            x = event.x()
+            y = event.y()
+
+            row = y // self.piece_size
+            col = x // self.piece_size
+
+            if board.is_valid_move(self.selected_piece_pos, [row, col]):
+                board.update_position(self.selected_piece_pos, [row, col])
+                self.selected_piece = None
+                self.selected_piece_pos = None
+                self.update()
+            else:
+                self.selected_piece = None
+                self.selected_piece_pos = None
+                self.update()
+
+
+app = QApplication(sys.argv)
+ex = CheckersBoard()
+sys.exit(app.exec_())
+
+"""
 run = True
 board = Board()
 move = [[None, None], [None, None]]
@@ -9,3 +109,4 @@ while run:
     print(board.print_board())
     print(board.piece_moves(2, 1))
     input() 
+"""
--- code/board.py
+++ code/board.py
@@ -24,6 +24,20 @@ class Board:
             [2, -1, 2, -1, 2, -1, 2, -1]
        ]
 
+    def is_valid_move(self, start, end):
+        moves = self.piece_moves(start[0], start[1])
+        if any(move == end for move in moves):
+            return True
+        jumps = self.piece_jumps(start[0], start[1])
+        if any(jump == end for jump in jumps):
+            return True
+        return False
+    
+    def update_position(self, start, end):
+        self.board[start[0], start[1]] 
+        return True
+
+
     def real_spot(self, row, col):
         if row >= 0 and row < 8:
             if col >= 0 and col < 8:
@@ -34,16 +48,19 @@ class Board:
     def piece_moves(self, row, col):
         moves = []
         piece = self.board[row][col]
+
         if piece == self.P1:
             if (col+1 < 8 and self.board[row+1][col+1] == 0):
                 moves.append([row+1, col+1])
             if (col-1 >= 0 and self.board[row+1][col-1] == 0):
                 moves.append([row+1, col-1])
+
         elif piece == self.P2:
             if (col+1 < 8 and self.board[row-1][col+1] == 0):
                 moves.append([row-1, col+1])
             if (col-1 >= 0 and self.board[row-1][col-1] == 0):
                 moves.append([row-1, col-1])
+
         elif piece == self.P1_K or piece == self.P2_K:
             if row-1 >= 0:
                 if (col+1 < 8 and self.board[row-1][col+1] == 0):
@@ -57,32 +74,76 @@ class Board:
                     moves.append([row+1, col-1])
         return moves
     
+
     def piece_jumps(self, row, col):
         jumps = []
         piece = self.board[row][col]
         if piece == self.P1:
-            if (col+1 < 8 and self.board[row+1][col+1] == 0):
-                jumps.append([row+1, col+1])
-            if (col-1 >= 0 and self.board[row+1][col-1] == 0):
-                jumps.append([row+1, col-1])
+            if row+2 < 8:
+                if (col+2 < 8 and row+2 < 8
+                    and (self.board[row+1][col+1] == self.P2 or self.board[row+1][col+1] == self.P2_K) 
+                    and self.board[row+2][col+2] == self.EMPTY_SPOT):
+                    jumps.append([row+2, col+2])
+
+                if (col-2 >= 0 and row+2 < 8
+                    and (self.board[row+1][col-1] == self.P2 or self.board[row+1][col-1] == self.P2_K)
+                    and self.board[row+2][col-2] == self.EMPTY_SPOT):
+                    jumps.append([row+2, col-2])
+
         elif piece == self.P2:
-            if (col+1 < 8 and self.board[row-1][col+1] == 0):
-                jumps.append([row-1, col+1])
-            if (col-1 >= 0 and self.board[row-1][col-1] == 0):
-                jumps.append([row-1, col-1])
-        elif piece == self.P1_K or piece == self.P2_K:
+            if row-2 >= 0:
+                if (col+2 < 8
+                    and (self.board[row-1][col+1] == self.P1 or self.board[row-1][col+1] == self.P1_K)
+                    and self.board[row-2][col+2] == self.EMPTY_SPOT):
+                    jumps.append([row-2, col+2])
+                if (col-2 >= 0
+                    and (self.board[row-1][col-1] == self.P1 or self.board[row-1][col-1] == self.P1_K)
+                    and self.board[row-2][col-2] == self.EMPTY_SPOT):
+                    jumps.append([row-2, col-2])
+
+        elif piece == self.P1_K:
             if row-1 >= 0:
-                if (col+1 < 8 and self.board[row-1][col+1] == 0):
-                    jumps.append([row-1, col+1])
-                if (col-1 >= 0 and self.board[row-1][col-1] == 0):
-                    jumps.append([row-1, col-1])
+                if (col+1 < 8 
+                    and (self.board[row-1][col+1] == self.P2 or self.board[row-1][col+1] == self.P2_K)
+                    and self.board[row-2][col+2] == self.EMPTY_SPOT):
+                    jumps.append([row-2, col+2])
+                if (col-1 >= 0 
+                    and (self.board[row-1][col-1] == self.P2 or self.board[row-1][col-1] == self.P2_K)
+                    and self.board[row-2][col-2] == self.EMPTY_SPOT):
+                    jumps.append([row-2, col-2])
+
             if row+1 < 8:
-                if (col+1 < 8 and self.board[row+1][col+1] == 0):
-                    jumps.append([row+1, col+1])
-                if (col-1 >= 0 and self.board[row+1][col-1] == 0):
-                    jumps.append([row+1, col-1])
+                if (col+1 < 8 
+                    and (self.board[row+1][col+1] == self.P2 or self.board[row+1][col+1] == self.P2_K)
+                    and self.board[row+2][col+2] == self.EMPTY_SPOT):
+                    jumps.append([row+2, col+2])
+                if (col-1 >= 0 
+                    and (self.board[row+1][col-1] == self.P2 or self.board[row+1][col-1] == self.P2_K)
+                    and self.board[row+2][col-2] == self.EMPTY_SPOT):
+                    jumps.append([row+2, col-2])
+
+        elif piece == self.P2_K:
+            if row-1 >= 0:
+                if (col+1 < 8 
+                    and (self.board[row-1][col+1] == self.P1 or self.board[row-1][col+1] == self.P1_K)
+                    and self.board[row-2][col+2] == self.EMPTY_SPOT):
+                    jumps.append([row-2, col+2])
+                if (col-1 >= 0 
+                    and (self.board[row-1][col-1] == self.P1 or self.board[row-1][col-1] == self.P1_K)
+                    and self.board[row-2][col-2] == self.EMPTY_SPOT):
+                    jumps.append([row-2, col-2])
+            if row+1 < 8:
+                if (col+1 < 8 
+                    and (self.board[row+1][col+1] == self.P1 or self.board[row+1][col+1] == self.P1_K)
+                    and self.board[row+2][col+2] == self.EMPTY_SPOT):
+                    jumps.append([row+2, col+2])
+                if (col-1 >= 0 
+                    and (self.board[row+1][col-1] == self.P1 or self.board[row+1][col-1] == self.P1_K)
+                    and self.board[row+2][col-2] == self.EMPTY_SPOT):
+                    jumps.append([row+2, col-2])
         return jumps
     
+
     def possible_moves(self):
         for row in range(8):
             for col in range(8):
@@ -91,9 +152,7 @@ class Board:
                     self.piece_moves(piece, row, col)
                     
         
-    def update_position(self, start, end):
-        return True
-
+    
     def print_board(self):
         for row in self.board:
             for square in row:
@@ -110,7 +169,3 @@ class Board:
                 elif square == 4:
                     print("\u2654 ", end="")
             print()
-        
-
-
-
